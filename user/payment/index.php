89fffd4644dcd6c8892ee8b547b0c9f5f62a4c65
Toby <32206025+tobyabraham@users.noreply.github.com>
2021-06-22 10:55:19 +0100

    Update index.php

--- user/payment/index.php
+++ user/payment/index.php
@@ -2,7 +2,7 @@
 session_start();
 date_default_timezone_set('Africa/Lagos');
 $email = $_SESSION['user'];
-$conn=mysqli_connect('localhost','root','','wecoin') or die('Could not Connect My Sql:'.mysql_error());
+$conn=mysqli_connect('remotemysql.com','AI9hgEKDPt','nS6lsKdyHE','AI9hgEKDPt') or die('Could not Connect My Sql:'.mysql_error());
 if(!isset($_SESSION['user'])){
     header("location: /wecoin/login");  
 }

8e3d002e1bb2207fa8b18ea60abd3e1b79a565fa
Toby <32206025+tobyabraham@users.noreply.github.com>
2021-06-22 19:08:33 +0100

    Update index.php

--- user/tps/index.php
+++ user/tps/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 date_default_timezone_set('Africa/Lagos');
-$conn=mysqli_connect('localhost','root','','wecoin') or die('Could not Connect My Sql:'.mysql_error());
+$conn=mysqli_connect('remotemysql.com','AI9hgEKDPt','nS6lsKdyHE','AI9hgEKDPt') or die('Could not Connect My Sql:'.mysql_error());
 
 if(isset($_SESSION['user'])){
     if(isset($_POST['search'])){

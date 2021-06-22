61eb9ee7f8f262e35f2c7c3aab5bf901f247e7be
Toby <32206025+tobyabraham@users.noreply.github.com>
2021-06-22 09:45:48 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect('localhost','root','','wecoin') or die('Could not Connect My Sql:'.mysql_error());    
+$conn=mysqli_connect('remotemysql.com','AI9hgEKDPt','nS6lsKdyHE','AI9hgEKDPt') or die('Could not Connect My Sql:'.mysql_error());    
 
 ?>
 

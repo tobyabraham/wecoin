17847aa288102f968cc4282176c7bc827d5d4283
JOSH <53708344+Josh-Algors@users.noreply.github.com>
2021-06-24 14:17:48 +0100

    Update TRX_USDT.php

--- user/margin/TRX_USDT.php
+++ user/margin/TRX_USDT.php
@@ -27,7 +27,7 @@ if(isset($_SESSION["user"])){
     $fname = $fname[0];
     $_SESSION['lastlogin'] = $row['last login'];
     
-    if(isset($_POST["options"])){
+   if(isset($_POST["options"])){
         $nrows = $_POST["options"] - 1;
         $rs=mysqli_query($conn, "SELECT * FROM `openorder` WHERE `Trade Type` LIKE '%MARGIN%'  LIMIT $nrows,1");
         $rowa = mysqli_fetch_array($rs);
@@ -54,7 +54,7 @@ if(isset($_SESSION["user"])){
                 }
                 $profitloss = array_merge($profit,$loss);
             shuffle($profitloss);
-        $numofdays = $openday - date("d");
+        $numofdays = date("d") - $openday;
         for ($z = 0; $z < ($numofdays + 1); $z++) {
             $levresult = $levresult + ($levresult*($profitloss[$z]/100));
             
